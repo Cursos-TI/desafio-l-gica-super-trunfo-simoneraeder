d1aa024d0c064a4b0cbae7e361e46c7f4c86e8a3
simoneraeder <166766334+simoneraeder@users.noreply.github.com>
2025-03-01 16:32:27 +0000

    case

--- logicaSuperTrunfoMestre.c
+++ logicaSuperTrunfoMestre.c
@@ -6,6 +6,53 @@ int main() {
 
     printf("Lógica Super Trunfo, Mestre!\n \t");
 
+int resultado1, resultado2;
+char primeiroAtributo, segundoAtributo;
+int pontoTuristico1, pontoTuristico2, populacao1, populacao2, area1, area2;
+
+//Gerar número aleatório
+
+srand(time(0));
+
+pontoTuristico1 = 10;
+pontoTuristico2 = 20;
+densidade_populacional = 94;
+densidade_populacional2 = 32;
+area1 = 79;
+area2 = 53;
+
+// Menu interativo, iniciando o jogo.
+
+printf("******* Bem vindo, ao Jogo!!****\n");
+printf("Escolha o Primeiro atributo:\n");
+printf("P. Ponto turístico \n");
+printf("D. Densidade Populacional \n");
+printf("A. Área\n");
+
+printf("Escolha a comparação: \n");
+scanf("%c", &primeiroAtributo);
+
+switch (primeiroAtributo)
+{
+case 'P'
+     'p'
+     printf("Você escolheu a opção Ponto Turístico"\n);
+     resultado1 = pontoTuristico2 > pontoTuristico1 ? 1 : 0 ;
+    break;
+
+    case 'D'
+     'd'
+     printf("Você escolheu a opção Ponto Turístico"\n);
+     resultado1 =  
+    break;
+
+
+default:
+    break;
+}
+
+
+
 
 
 
